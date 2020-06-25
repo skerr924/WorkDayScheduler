0ae1d2558fc32c5474702daac47467b8387e9777
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-25 18:39:32 -0400

    added code to pull currentHour, will be used in function which grays out time blocks which have already occured

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ $(document).ready(function() {
 
 var timeNowDisplay; 
 var timeNow; 
+var currentHour; 
 
 updateTime();
+// alreadyPassed();
+
+
 //function to continuously update the date and time 
 function updateTime () {
     timeNowDisplay = $("#currentDay"); //option 1 
     timeNow = moment().format('MMMM Do YYYY, h:mm a');
     timeNowDisplay.text("The time now is: " + timeNow); 
+    currentHour = moment().format('h');
+    console.log(currentHour);
 }
 setInterval(updateTime, 1000);
+
+// function alreadyPassed (){
+
+// }
 
 });
