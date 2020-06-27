12ce24f0fff10568b39029744bd1d0adb8d4bf38
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-27 11:13:27 -0400

    functions added to save events to local storage and pull from local storage, not yet functional

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 
 var timeNowDisplay; 
 var timeAndDateNow; 
-var buttonHour; 
+// var buttonHour; 
+var events = [];
 // var clockTime; 
 
 
@@ -42,12 +43,58 @@ function compareTime () {
 }
 
 function saveEvent() {
+    event.preventDefault();
     buttonHour = $(this).attr('id');
     var parent = $(this).parent();
     var grandparent = parent.parent();
-    console.log(grandparent.find("textarea")[0].value);
+    var newEvent = grandparent.find("textarea")[0].value;
+    events.push(newEvent);
+    storeEvent();
+    getStoredEvents();
   }
 
-  $(".saveBtn").on("click", saveEvent);
+// function storeEvent() {
+//     // Stringify and set each event item in localStorage to
+//     localStorage.setItem("events", JSON.stringify(events));
+//   }
 
-});
+// //get stored events from local storage 
+// function getStoredEvents(){
+//     console.log("get stored events");
+//     var storedEvents = JSON.parse(localStorage.getItem("events")); 
+//     if (storedEvents !== null){
+//         events = storedEvents; 
+//     }
+
+//     renderEvents(); 
+// }
+
+// function renderEvents(){
+//     console.log(events);
+// }
+
+// $(".saveBtn").on("click", saveEvent);
+
+// });
+
+  
+//   //all event listeners below: 
+
+//   highScoreForm.addEventListener("submit", function(event) {
+//       event.preventDefault();
+    
+//       var nameText = nameInput.value.trim() + " with a score of " + finalScore;
+    
+//       // Return from function early if submitted nameText is blank
+//       if (nameText === "") {
+//         return;
+//       }
+  
+//       // Add new nameText to names array, clear the input
+//       names.push(nameText);
+//       nameInput.value = "";
+    
+//       // Store updated todos in localStorage, re-render the list
+//       storeNames();
+//       renderHighScores();
+//   });
